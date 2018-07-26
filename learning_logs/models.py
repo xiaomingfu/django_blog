c27ba7af6675e86f43d3f258f2e2aaf8eff63c7f
xiaomingfu <xmingfu86@gmail.com>
2018-07-26 14:47:07 -0700

    create an app and define models

---
+++ learning_logs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Topic(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200)
+    data_added = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+    """Return a string representation of the model."""
+    return self.text
+
