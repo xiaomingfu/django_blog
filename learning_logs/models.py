4433b38ff4981db1ad0ee3902329162c6c187b40
xiaomingfu <xmingfu86@gmail.com>
2018-07-26 15:08:14 -0700

    define the entry model and migrate the database

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -13,3 +13,15 @@ def __str__(self):
     """Return a string representation of the model."""
     return self.text
 
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    data_add = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text[:50] + "..."
