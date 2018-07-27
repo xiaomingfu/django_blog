d182acfe4ae74e90b83e4dd246b100daec93ec09
xiaomingfu <xmingfu86@gmail.com>
2018-07-26 17:14:02 -0700

    add topics page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,9 @@ app_name = 'learning_logs'
 urlpatterns = [
     #Home page
     path('', views.index, name='index'),
+    #Show all topics
+    path('topics/', views.topics, name='topics'),
+    path('topic/', views.topic, name='topic'),
+    path('entries/', views.entries, name='entries'),
+    path('entry/', views.entry, name='entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from learning_logs.models import Topic, Entry
+
 # Create your views here.
 def index(request):
     """The home page for Learning log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request):
+    pass
+
+def entries(request):
+    pass
+
+def entry(request):
+    pass
