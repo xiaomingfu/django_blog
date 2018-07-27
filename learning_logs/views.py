51e9610f1c5530b8c6bc02c66c9def2dd45e1d8b
xiaomingfu <xmingfu86@gmail.com>
2018-07-26 21:16:11 -0700

    add edit entry page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -60,3 +60,26 @@ def new_entry(request, topic_id):
                                                 args=[topic_id]))
     context={'form': form, 'topic': topic}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        #POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse(
+                'learning_logs:topic', args=[topic.id]
+            ))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
+
