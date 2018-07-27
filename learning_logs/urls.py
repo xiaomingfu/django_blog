2b4cce5ed01f944e6bea6be520f3467b80854cd9
xiaomingfu <xmingfu86@gmail.com>
2018-07-26 18:19:43 -0700

    Add new topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,12 +5,13 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    #Home page
+    # Home page
     path('', views.index, name='index'),
-    #Show all topics
+    # Show all topics
     path('topics/', views.topics, name='topics'),
-    #Detail page for a single topic
+    # Detail page for a single topic
     path('topics/<int:topic_id>/', views.topic, name='topic'),
-    path('entries/', views.entries, name='entries'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
     path('entry/', views.entry, name='entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from learning_logs.models import Topic, Entry
+from .forms import TopicForm
+
 
 # Create your views here.
 def index(request):
     """The home page for Learning log"""
     return render(request, 'learning_logs/index.html')
 
+
 def topics(request):
     """Show all topics"""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
 
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
@@ -20,8 +26,22 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
-def entries(request):
-    pass
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        #POST data submiited; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
 
 def entry(request):
     pass
